7ace12f3920ad6922e3e740fd2c96041315a0cb8
rugieri <rugieri.ronaldo@gmail.com>
2022-01-27 20:04:32 -0300

    searching for videos

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import SearchBar from './SearchBar';
-
+import youtube from './apis/youtube';
 class App extends React.Component {
+  onTermSubmit = (term) => {
+    youtube.get('/search', {
+      param: {
+        q: term,
+      },
+    });
+  };
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
       </div>
     );
   }
--- src/components/apis/youtube.js
+++ src/components/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyBjlzqp7XuQGjBS-8PabnPxx59RrT6DQ8c';
+
+export default axios.create({
+  baseURL: 'http://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    type: 'video',
+    maxResults: 5,
+    key: KEY,
+  },
+});
